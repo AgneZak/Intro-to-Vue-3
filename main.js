bbb8fd69aeb15dea865cb292eaeab8abdd883e0c
Engaz01 <azakaite@github.com>
2021-01-28 14:05:42 +0200

    Add remove button

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    remove(id) {
+        this.cart = this.cart.splice(id, 1);
+    }
+  },
+});
