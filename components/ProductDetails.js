ae7f5b7fe7ac22e46ac65f6032f21f369bd29863
Engaz01 <azakaite@github.com>
2021-01-28 13:28:16 +0200

    Add to other component

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -4,5 +4,10 @@ app.component("product-details", {
       type: Array,
       required: true,
     },
+    template:
+      /* html */
+      `<ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>`,
   },
 });
